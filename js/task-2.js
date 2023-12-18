03a21bae64065a439d9e7e3a45e270c532b0c419
Oleksandr <androleks@gmail.com>
2023-12-18 20:47:22 +0300

    task-2 completed

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,13 @@
-Оголоси функцію formatMessage(message, maxLength), яка приймає рядок (параметр message) та перевіряє його довжину відповідно до заданої максимальної довжини (параметр maxLength).
+function formatMessage(message, maxLength) {
+    // if(message.length <= maxLength) {
+    // return(message);
+    // } else { return(`${message.slice(0, maxLength)}...`); }
 
-Доповни код функції таким чином, що:
+    const formatMessage = message.length <= maxLength ? message : `${message.slice(0, maxLength)}...`;
+    return (formatMessage); 
+    // закоментований метод вище також працює. більш сподобався цей:)
 
-Якщо довжина рядка дорівнює або менша за maxLength, то функція повертає початковий рядок без змін.
-Якщо довжина перевищує maxLength, то функція обрізає рядок до maxLength символів, додає трикрапку "..." в кінці та повертає обрізану версію.
-Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+ }
 
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
@@ -12,5 +15,3 @@ console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum
 console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
 console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
 console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
-
-
